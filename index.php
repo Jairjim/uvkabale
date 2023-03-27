0eced70ce0c3082f793565bf1ca708991c6483a7
Jair Josué Jimarez García <69481164+Jairjim@users.noreply.github.com>
2023-03-27 12:24:40 -0600

    Cmbio de imagen

--- index.php
+++ index.php
@@ -10,10 +10,15 @@
     <link rel="stylesheet" href="indexCSS.css">
     <link rel="icon" type="image/png" href="logo.png">
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+        integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
+        crossorigin="anonymous" />
     <!-- Bootstrap v5 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
@@ -27,7 +32,7 @@
 
 <body>
     <!-- Imagen de inicio -->
-    <section id="section01" class="demo">
+    <section class="demo">
         <div class="row justify-content-end">
             <div class="col-4">
                 <p class="slogan">
@@ -44,7 +49,8 @@
     <nav class="navbar navbar-expand-lg navbar-light azul scrolling-navbar fixed-top">
         <div class="container">
             <a class="navbar-brand" href="https://uvkabale.com"><img src="logo.png" width="180" height="90"></a>
-            <button class="navbar-toggler bt" type="button" data-toggle="collapse" data-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler bt" type="button" data-toggle="collapse" data-target="#navbarScroll"
+                aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse justify-content-end" id="navbarScroll">
@@ -56,7 +62,8 @@
                         <a class="nav-link page" href="Servicios"> Servicios </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link contact" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"> Contáctanos </a>
+                        <a class="nav-link contact" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
+                            aria-controls="offcanvasRight"> Contáctanos </a>
                     </li>
                 </ul>
             </div>
@@ -69,19 +76,22 @@
             <div class="container-fluid justify-content-center gap-5 container-tweet">
                 <div class="card" style="width: 18rem;">
                     <div class="card-body card-tweet">
-                        <a class="twitter-timeline" href="https://twitter.com/AduanasMx?s=20&t=G2ZBb7qmLqSwt-fsXEw1Lg" data-tweet-limit="1" data-width="399"></a>
+                        <a class="twitter-timeline" href="https://twitter.com/AduanasMx?s=20&t=G2ZBb7qmLqSwt-fsXEw1Lg"
+                            data-tweet-limit="1" data-width="399"></a>
                         <script async src="http://platform.twitter.com/widgets.js" charset="utf-8"></script>
                     </div>
                 </div>
                 <div class="card" style="width: 18rem;">
                     <div class="card-body card-tweet">
-                        <a class="twitter-timeline" href="https://twitter.com/SNICEmx" data-tweet-limit="1" data-width="399"></a>
+                        <a class="twitter-timeline" href="https://twitter.com/SNICEmx" data-tweet-limit="1"
+                            data-width="399"></a>
                         <script async src="http://platform.twitter.com/widgets.js" charset="utf-8"></script>
                     </div>
                 </div>
                 <div class="card" style="width: 18rem;">
                     <div class="card-body card-tweet">
-                        <a class="twitter-timeline" href="https://twitter.com/SATMX" data-tweet-limit="1" data-width="399"></a>
+                        <a class="twitter-timeline" href="https://twitter.com/SATMX" data-tweet-limit="1"
+                            data-width="399"></a>
                         <script async src="http://platform.twitter.com/widgets.js" charset="utf-8"></script>
                     </div>
                 </div>
@@ -93,8 +103,11 @@
             <p> Nosotros... </p>
         </div>
         <div class="row about">
-            Kabale es un organismo integrado por profesionales con más de 20 años de experiencia en Comercio Exterior, donde además de evaluar Normatividad Nacional e Internacional en más de 8 sectores productivos, asistimos a los fabricantes nacionales e importadores
-            que requieren evaluar la conformidad de sus productos contra las Normas Oficiales Mexicanas de información comercial...
+            Kabale es un organismo integrado por profesionales con más de 20 años de experiencia en Comercio Exterior,
+            donde además de evaluar Normatividad Nacional e Internacional en más de 8 sectores productivos, asistimos a
+            los fabricantes nacionales e importadores
+            que requieren evaluar la conformidad de sus productos contra las Normas Oficiales Mexicanas de información
+            comercial...
         </div>
         <div class="boton-about">
             <a class="btn" href="Nosotros"> Conocer más <i class="bi bi-arrow-right-short"></i></a>
@@ -105,7 +118,9 @@
             <p> Servicios... </p>
         </div>
         <div class="row about">
-            Contamos con los recursos humanos y materiales para atender los requerimientos de nuestros clientes, reduciendo tiempos y los trabajos en el proceso de Evaluación.<br> Además, tenemos la aprobación por parte de la Autoridad, para evaluar en
+            Contamos con los recursos humanos y materiales para atender los requerimientos de nuestros clientes,
+            reduciendo tiempos y los trabajos en el proceso de Evaluación.<br> Además, tenemos la aprobación por parte
+            de la Autoridad, para evaluar en
             depósito fiscal y domicilio particular del Importador.
         </div>
         <div class="boton-about">
@@ -222,7 +237,8 @@
                 </a>
                 <div class="card__info">
                     <span class="card__category"> NOM-186-SSA1/SCFI-2013 </span>
-                    <h3 class="card__title"> Etiquetado de Cacao, chocolate y productos similares, y derivados del cacao</h3>
+                    <h3 class="card__title"> Etiquetado de Cacao, chocolate y productos similares, y derivados del cacao
+                    </h3>
                 </div>
             </article>
             <article class="card card--11">
@@ -253,21 +269,29 @@
 
     <!-- Redes Sociales  -->
     <div class="share-btn-container">
-        <a href="http://www.linkedin.com/in/unidad-de-inspecci%C3%B3n-kabale-s-a-de-c-v-7b915a234" target="_blank" class="linkedin-btn"><i class="fab fa-linkedin"></i></a>
-        <a href="https://www.instagram.com/uikabale/" class="instagram-btn" target="_blank"><i class="fab fa-instagram"></i></a>
-        <a href="https://wa.me/message/ECFFKO55QMCEG1" class="whatsapp-btn" target="_blank"><i class="fab fa-whatsapp"></i></a>
-        <a href="https://twitter.com/KabaleUi?t=AYX-oA3kvUYJiJ_hLvPbtA&s=08" class="twitter-btn" target="_blank"><i class="fab fa-twitter"></i></a>
-        <a href="https://www.facebook.com/profile.php?id=100079046550116" class="facebook-btn" target="_blank"><i class="fab fa-facebook"></i></a>
+        <a href="http://www.linkedin.com/in/unidad-de-inspecci%C3%B3n-kabale-s-a-de-c-v-7b915a234" target="_blank"
+            class="linkedin-btn"><i class="fab fa-linkedin"></i></a>
+        <a href="https://www.instagram.com/uikabale/" class="instagram-btn" target="_blank"><i
+                class="fab fa-instagram"></i></a>
+        <a href="https://wa.me/message/ECFFKO55QMCEG1" class="whatsapp-btn" target="_blank"><i
+                class="fab fa-whatsapp"></i></a>
+        <a href="https://twitter.com/KabaleUi?t=AYX-oA3kvUYJiJ_hLvPbtA&s=08" class="twitter-btn" target="_blank"><i
+                class="fab fa-twitter"></i></a>
+        <a href="https://www.facebook.com/profile.php?id=100079046550116" class="facebook-btn" target="_blank"><i
+                class="fab fa-facebook"></i></a>
     </div>
 
     <!--Offcanvas-->
-    <div class="offcanvas offcanvas-end bg-dark" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+    <div class="offcanvas offcanvas-end bg-dark" tabindex="-1" id="offcanvasRight"
+        aria-labelledby="offcanvasRightLabel">
         <div class="offcanvas-header">
             <h5 class="text-white" id="offcanvasRightLabel"> Contáctanos </h5>
-            <button type="button" class="btn-close btn-close-white text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            <button type="button" class="btn-close btn-close-white text-reset" data-bs-dismiss="offcanvas"
+                aria-label="Close"></button>
         </div>
         <p class="text-white text-center"> Puedes comunicarte con nosotros al
-            <a target="_blank" href="https://wa.me/message/ECFFKO55QMCEG1">55 3245 9292</a> o bien llenar el siguiente formulario y uno de nuestros operadores se pondrá encontacto contigo en breve
+            <a target="_blank" href="https://wa.me/message/ECFFKO55QMCEG1">55 3245 9292</a> o bien llenar el siguiente
+            formulario y uno de nuestros operadores se pondrá encontacto contigo en breve
         </p>
 
         <div class="offcanvas-body ">
@@ -293,7 +317,8 @@
                         </div>
                         <br>
                         <div class="form-floating form-control-sm mb-3 ">
-                            <input name="tel" type="tel " class="form-control " placeholder="tel " maxlength="10" required>
+                            <input name="tel" type="tel " class="form-control " placeholder="tel " maxlength="10"
+                                required>
                             <label for="tel"> Numero de teléfono </label>
                         </div>
                         <div class="modal-footer ">
@@ -311,19 +336,27 @@
         <div class="container pt-4 ">
             <section class="mb-4 ">
                 <!-- Redes Sociales -->
-                <a class="btn btn-link btn-floating btn-lg text-dark m-1 " href="https://wa.me/message/ECFFKO55QMCEG1" role="button " data-mdb-ripple-color="dark ">
+                <a class="btn btn-link btn-floating btn-lg text-dark m-1 " href="https://wa.me/message/ECFFKO55QMCEG1"
+                    role="button " data-mdb-ripple-color="dark ">
                     <i class="bi bi-whatsapp "></i>
                 </a>
-                <a class="btn btn-link btn-floating btn-lg text-dark m-1 " target="_blank " href="https://twitter.com/KabaleUi?t=AYX-oA3kvUYJiJ_hLvPbtA&s=08 " role="button " data-mdb-ripple-color="dark ">
+                <a class="btn btn-link btn-floating btn-lg text-dark m-1 " target="_blank "
+                    href="https://twitter.com/KabaleUi?t=AYX-oA3kvUYJiJ_hLvPbtA&s=08 " role="button "
+                    data-mdb-ripple-color="dark ">
                     <i class="bi bi-twitter "></i>
                 </a>
-                <a class="btn btn-link btn-floating btn-lg text-dark m-1 " href="https://www.instagram.com/uikabale/ " role="button " data-mdb-ripple-color="dark ">
+                <a class="btn btn-link btn-floating btn-lg text-dark m-1 " href="https://www.instagram.com/uikabale/ "
+                    role="button " data-mdb-ripple-color="dark ">
                     <i class="bi bi-instagram "></i>
                 </a>
-                <a class="btn btn-link btn-floating btn-lg text-dark m-1 " href="http://www.linkedin.com/in/unidad-de-inspecci%C3%B3n-kabale-s-a-de-c-v-7b915a234 " role="button " data-mdb-ripple-color="dark ">
+                <a class="btn btn-link btn-floating btn-lg text-dark m-1 "
+                    href="http://www.linkedin.com/in/unidad-de-inspecci%C3%B3n-kabale-s-a-de-c-v-7b915a234 "
+                    role="button " data-mdb-ripple-color="dark ">
                     <i class="bi bi-linkedin "></i>
                 </a>
-                <a class="btn btn-link btn-floating btn-lg text-dark m-1 " href="https://www.facebook.com/profile.php?id=100079046550116" role="button " data-mdb-ripple-color="dark ">
+                <a class="btn btn-link btn-floating btn-lg text-dark m-1 "
+                    href="https://www.facebook.com/profile.php?id=100079046550116" role="button "
+                    data-mdb-ripple-color="dark ">
                     <i class="bi bi-facebook "></i>
                 </a>
             </section>
@@ -344,20 +377,20 @@
 
     error_reporting(E_ERROR | E_PARSE);
     $conexion = mysqli_connect("localhost", "u899500393_jairjimarez200", "JairjimUNI21", "u899500393_Cliente");
-    if (!$conexion){
+    if (!$conexion) {
         die("Connection failed: " . mysqli_connect_error());
     }
     $nombre = $_POST["nombre"];
     $email = $_POST["email"];
     $empresa = $_POST["empresa"];
     $tel = $_POST["tel"];
-    if(isset($nombre)){
-    $query = "insert into usuario(Nombre, Correo, Empresa, Telefono) values('$nombre', '$email', '$empresa', '$tel')";
+    if (isset($nombre)) {
+        $query = "insert into usuario(Nombre, Correo, Empresa, Telefono) values('$nombre', '$email', '$empresa', '$tel')";
         if (mysqli_query($conexion, $query)) {
             echo "<script type='text/javascript'>
                     window.location.href='index.php'
                   </script>";
-        }else {
+        } else {
             echo "Error: " . $query . "<br>" . mysqli_error($conexion);
         }
     }
@@ -368,11 +401,14 @@
     <!-- JQuery -->
     <script type="text/javascript " src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js "></script>
     <!-- Bootstrap tooltips -->
-    <script type="text/javascript " src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js "></script>
+    <script type="text/javascript "
+        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js "></script>
     <!-- Bootstrap core JavaScript -->
-    <script type="text/javascript " src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js "></script>
+    <script type="text/javascript "
+        src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js "></script>
     <!-- MDB core JavaScript -->
-    <script type="text/javascript " src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js "></script>
+    <script type="text/javascript "
+        src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js "></script>
 </body>
 
 </html>
